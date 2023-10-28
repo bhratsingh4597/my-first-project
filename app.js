877536b0aca78199c9b118df060313426d7e1e13
Bhrat <bhratsingh4597>
2023-10-28 12:29:29 +0530

    remove react element and add jsx

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
 import React from "react";
 import  ReactDOM  from "react-dom/client";
 
-const heading1=React.createElement(
-    "h1",
-    {
-        id:"title"
-    },
-"Namaste React");
-
-const heading2=React.createElement(
-    "h3",
-    {
-        id:"paragraph"
-    },
-"Namaste React");
-
-const container=React.createElement(
-    "div",
-    {
-        id:"container"
-    },
-    [heading1,heading2]
+const heading1= (
+    <h1 id="title" key="h2">
+     "Namaste React"
+     </h1>
 );
+
+const HeaderComponent= ()=>{
+    return (
+        <div> 
+            {heading1}
+           <h2>
+            Namaste React Component
+           </h2>
+           <p>This is paragraph</p>
+        </div>
+    )
+}
+
 console.log(heading1)
 const rend =ReactDOM.createRoot(document.getElementById("root"));
-console.log(rend)
-rend.render(container);
+// console.log(rend)
+rend.render(<HeaderComponent />);
